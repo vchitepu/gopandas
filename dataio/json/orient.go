ef25e0710392e6f1e7dca130efb6d7ba61e23748
Vinay Chitepu <vinaychitepu@gmail.com>
2026-04-08 00:20:59 -0400

    feat(dataio/json): add JSONOrient enum type

---
+++ dataio/json/orient.go
@@ -0,0 +1,26 @@
+package json
+
+// JSONOrient specifies the JSON structure format for serialization/deserialization.
+type JSONOrient int
+
+const (
+	// OrientRecords: [{col: val, ...}, ...] (default)
+	OrientRecords JSONOrient = iota
+	// OrientColumns: {col: [val, ...], ...}
+	OrientColumns
+	// OrientIndex: {idx: {col: val, ...}, ...}
+	OrientIndex
+)
+
+func (o JSONOrient) String() string {
+	switch o {
+	case OrientRecords:
+		return "records"
+	case OrientColumns:
+		return "columns"
+	case OrientIndex:
+		return "index"
+	default:
+		return "unknown"
+	}
+}
